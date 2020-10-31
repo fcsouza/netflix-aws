0147a4adcb1f6671418074c209e1e9f0d3683942
fcsouza <fcsouza18@hotmail.com>
2020-10-31 12:59:20 -0300

    include properResolution

--- src/public/bandersnatch/src/network.js
+++ src/public/bandersnatch/src/network.js
@@ -11,4 +11,29 @@ class Network {
         const response = await fetch(url)
         return response.arrayBuffer()
     }
+
+    async getProperResolution(url) {
+        const startMs = Date.now()
+        const response = await fetch(url)
+        await response.arrayBuffer()
+        const endMs = Date.now()
+        const durationInMs = (endMs - startMs)
+        console.log('durationInMs',durationInMs)
+
+        const resolutions = [
+            { start: 3001, end: 20000, resolution: 144},
+            { start: 901, end: 3000, resolution: 360},
+            { start: 0, end: 900, resolution: 720},
+        ]
+
+        const item = resolutions.find(item => {
+            return item.start <= durationInMs && item.end >= durationInMs
+        })
+
+        const LOWEST_RESOLUTION = 144
+        // se for mais de 30s
+        if(!item) return LOWEST_RESOLUTION
+
+        return item.resolution
+    }
 }
